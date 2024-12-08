6b6e08530acef40f3e029b991e7cd8307fb0cbd4
astrawebbiz <astra.web.biz@gmail.com>
2024-12-08 17:39:59 +0300

    feat: logo link delete on main

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,4 +1,4 @@
-import {Link} from 'react-router-dom';
+import {Link, useLocation} from 'react-router-dom';
 import {AuthorizationStatus} from '../../const';
 import Nav from '../../components/header/nav';
 
@@ -9,15 +9,17 @@ type HeaderProps = {
 }
 
 function Header({isNavShow, countFavorites, authorizationStatus = AuthorizationStatus.Unknown}: HeaderProps): JSX.Element {
+  const logo = <img className='header__logo' src='img/logo.svg' alt='6 cities logo' width='81' height='41' />;
+  const logoBlock = (useLocation().pathname !== '/' ?
+    <Link className="header__logo-link header__logo-link--active" to="/">{logo}</Link>
+    :
+    logo
+  );
   return (
     <header className='header'>
       <div className='container'>
         <div className='header__wrapper'>
-          <div className='header__left'>
-            <Link className="header__logo-link header__logo-link--active" to="/">
-              <img className='header__logo' src='img/logo.svg' alt='6 cities logo' width='81' height='41' />
-            </Link>
-          </div>
+          <div className='header__left'>{logoBlock}</div>
           {(isNavShow && countFavorites) && <Nav authorizationStatus={authorizationStatus} countFavorites={countFavorites}/>}
         </div>
       </div>
--- src/components/reviews-form/reviews-form.tsx
+++ src/components/reviews-form/reviews-form.tsx
@@ -3,30 +3,42 @@ import { ChangeEvent, FormEvent, Fragment, useState } from 'react';
 
 type ReviewsFormProps = {
   offerId: string;
-  onAddReview: (offerId: string, rating: number, review: string) => void;
 };
 
-function ReviewsForm({offerId, onAddReview}: ReviewsFormProps): JSX.Element {
-
+function ReviewsForm({offerId}: ReviewsFormProps): JSX.Element {
   const [formData, setFormData] = useState({
     rating: 0,
     review: '',
+    ratingDisabled: false,
+    reviewDisabled: false,
+    submitDisabled: true,
+    offerId: offerId
   });
   const handleFieldChange = (evt: ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
     const {name, value} = evt.target;
     setFormData({...formData, [name]: (name === 'rating' ? +value : value)});
+    if (formData.rating && (formData.review.length >= 50 && formData.review.length < 300)) {
+      setFormData({...formData, 'submitDisabled': false});
+    }
   };
   return (
     <form className='reviews__form form' action='#' method='post' onSubmit={(evt: FormEvent<HTMLFormElement>) => {
       evt.preventDefault();
-      onAddReview(offerId, formData.rating, formData.review);
+      setFormData({
+        rating: 0,
+        review: '',
+        ratingDisabled: true,
+        reviewDisabled: true,
+        submitDisabled: true,
+        offerId: offerId
+      });
     }}
     >
       <label className='reviews__label form__label' htmlFor='review'>Your review</label>
       <div className='reviews__rating-form form__rating'>
         {RATING_VALUES.map((value: string, key: number)=> (
           <Fragment key={`rating_${value}`}>
-            <input className='form__rating-input visually-hidden' name='rating' value={STARS_COUNT - key} id={`${STARS_COUNT - key}-stars`} type='radio' onChange={handleFieldChange}/>
+            <input className='form__rating-input visually-hidden' name='rating' value={STARS_COUNT - key} id={`${STARS_COUNT - key}-stars`} type='radio' checked={formData.rating === STARS_COUNT - key} onChange={handleFieldChange} disabled={formData.ratingDisabled}/>
             <label htmlFor={`${STARS_COUNT - key}-stars`} className='reviews__rating-label form__rating-label' title={value}>
               <svg className='form__star-image' width='37' height='33'>
                 <use xlinkHref='#icon-star'></use>
@@ -35,12 +47,12 @@ function ReviewsForm({offerId, onAddReview}: ReviewsFormProps): JSX.Element {
           </Fragment>
         ))}
       </div>
-      <textarea className='reviews__textarea form__textarea' id='review' name='review' placeholder='Tell how was your stay, what you like and what can be improved' onChange={handleFieldChange} value={formData.review}></textarea>
+      <textarea className='reviews__textarea form__textarea' id='review' name='review' placeholder='Tell how was your stay, what you like and what can be improved' onChange={handleFieldChange} value={formData.review} disabled={formData.reviewDisabled}></textarea>
       <div className='reviews__button-wrapper'>
         <p className='reviews__help'>
           To submit review please make sure to set <span className='reviews__star'>rating</span> and describe your stay with at least <b className='reviews__text-amount'>50 characters</b>.
         </p>
-        <button className='reviews__submit form__submit button' type='submit'>Submit</button>
+        <button className='reviews__submit form__submit button' type='submit' disabled={formData.submitDisabled}>Submit</button>
       </div>
     </form>
   );
