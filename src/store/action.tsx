cd5481e48836a77403c86f793b5b6ee6db606e88
astrawebbiz <astra.web.biz@gmail.com>
2024-12-11 22:15:05 +0300

    fix: pr correct bug ts in reducer

--- src/store/action.tsx
+++ src/store/action.tsx
@@ -1,7 +1,8 @@
 import {createAction} from '@reduxjs/toolkit';
+import { SortTypes } from '../const';
 
 export const changeCity = createAction<string>('changeCity');
 
 export const getOffers = createAction('getOffers');
 
-export const changeSort = createAction<string>('changeSort');
+export const changeSort = createAction<SortTypes>('changeSort');
