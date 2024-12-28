111832fee6ce633dab7d788bba6d8be4f6466294
astrawebbiz <astra.web.biz@gmail.com>
2024-12-28 19:53:08 +0300

    fix. pr build mistake

--- src/store/user-process/selectors.ts
+++ src/store/user-process/selectors.ts
@@ -3,4 +3,4 @@ import {AuthorizationStatus} from '../../const';
 import {AppState} from '../../types';
 
 export const selectAuthorizationStatus = (state: AppState): AuthorizationStatus => state[NameSpace.User].authorizationStatus;
-export const selectUserName = (state: AppState): string => state[NameSpace.User].userInfo && state[NameSpace.User].userInfo.email;
+export const selectUserName = (state: AppState): string | null => state[NameSpace.User].userInfo && state[NameSpace.User].userInfo.email;
