87eb4e07a49d9325731d3e3462c888cf628e0294
astrawebbiz <astra.web.biz@gmail.com>
2025-01-20 18:18:05 +0300

    feat: add new bookmark test

--- src/components/bookmark-button/bookmark-button.pretest.tsx
+++ src/components/bookmark-button/bookmark-button.pretest.tsx
@@ -1,18 +1,25 @@
-import {render, screen} from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import BookmarkButton from './bookmark-button';
-import { APIRoute, AuthorizationStatus } from '../../const';
+import { APIRoute, AppRoute, AuthorizationStatus } from '../../const';
 import { extractActionsTypes, makeFakeOfferFull, makeFakeStore, makeFakeUserData } from '../../mocks';
 import { withHistory, withStore } from '../../mock-component';
 import userEvent from '@testing-library/user-event';
 import { changeFavoriteAction } from '../../store/api-actions';
+import { createMemoryHistory, MemoryHistory } from 'history';
 
 describe('Component: BookmarkButton', () => {
+  let mockHistory: MemoryHistory;
+
+  beforeEach(() => {
+    mockHistory = createMemoryHistory();
+  });
+
   it('should render correctly when isFavorite true and user authorized', () => {
     const expectedText = 'In bookmarks';
     const isFavorite = true;
     const offerId = 'some-offer-id';
     const bookmarkButtonTestId = 'bookmark-button-container';
-    const { withStoreComponent } = withStore(<BookmarkButton isFavorite={isFavorite} offerId={offerId}/>, makeFakeStore({
+    const { withStoreComponent } = withStore(<BookmarkButton isFavorite={isFavorite} offerId={offerId} />, makeFakeStore({
       user: {
         authorizationStatus: AuthorizationStatus.Auth,
         isErrorInAuthRequest: false,
@@ -33,7 +40,7 @@ describe('Component: BookmarkButton', () => {
     const isFavorite = true;
     const offerId = 'some-offer-id';
     const bookmarkButtonTestId = 'bookmark-button-container';
-    const { withStoreComponent } = withStore(<BookmarkButton isFavorite={isFavorite} offerId={offerId}/>, makeFakeStore({
+    const { withStoreComponent } = withStore(<BookmarkButton isFavorite={isFavorite} offerId={offerId} />, makeFakeStore({
       user: {
         authorizationStatus: AuthorizationStatus.NoAuth,
         isErrorInAuthRequest: false,
@@ -54,7 +61,7 @@ describe('Component: BookmarkButton', () => {
     const isFavorite = false;
     const offerId = 'some-offer-id';
     const bookmarkButtonTestId = 'bookmark-button-container';
-    const { withStoreComponent } = withStore(<BookmarkButton isFavorite={isFavorite} offerId={offerId}/>, makeFakeStore({
+    const { withStoreComponent } = withStore(<BookmarkButton isFavorite={isFavorite} offerId={offerId} />, makeFakeStore({
       user: {
         authorizationStatus: AuthorizationStatus.Auth,
         isErrorInAuthRequest: false,
@@ -94,5 +101,23 @@ describe('Component: BookmarkButton', () => {
       changeFavoriteAction.fulfilled.type,
     ]);
 
+  });
+
+  it('should navigate to login-page when not authorized user clicked bookmark button', async () => {
+    const isFavorite = false;
+    const offerId = 'some-offer-id';
+    const { withStoreComponent } = withStore(<BookmarkButton isFavorite={isFavorite} offerId={offerId} />, makeFakeStore({
+      user: {
+        authorizationStatus: AuthorizationStatus.NoAuth,
+        isErrorInAuthRequest: false,
+        isErrorInCheckAuthRequest: false,
+        userInfo: null
+      }
+    }));
+
+    const preparedComponent = withHistory(withStoreComponent);
+    render(preparedComponent);
+    await userEvent.click(screen.getByRole('button'));
+    expect(mockHistory.location.pathname).toBe(AppRoute.Login);
   });
 });
---
+++ src/components/card/card.test.tsx
@@ -0,0 +1,27 @@
+import { render, screen } from '@testing-library/react';
+import { makeFakeOffer } from '../../mocks';
+import Card from './card';
+import { withHistory } from '../../mock-component';
+
+vi.mock('../../components/bookmark-button/bookmark-button', () => {
+  const mockBookmarkButton = () => <>This is mock BookmarkButton</>;
+  return {
+    default: mockBookmarkButton
+  };
+});
+
+describe('Component: Card', () => {
+  it('should render correctly', () => {
+    const cardContainerTestId = 'place-card-container';
+    const offer = makeFakeOffer();
+    const cardMouseLeaveHandler = vi.fn();
+    const cardMouseEnterHandler = vi.fn();
+
+    const preparedComponent = withHistory(<Card offer={offer} onCardMouseEnter={() => cardMouseEnterHandler(offer) as void} onCardMouseLeave={cardMouseLeaveHandler} />);
+
+    render(preparedComponent);
+    const cardContainer = screen.getByTestId(cardContainerTestId);
+
+    expect(cardContainer).toBeInTheDocument();
+  });
+});
